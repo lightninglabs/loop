ba1f427cf45d8e7e3d38c7c8788d02fe840a1f79
Olaoluwa Osuntokun <laolu32@gmail.com>
2019-03-06 15:38:36 -0800

    looprpc: move client+server protos to new looprpc package

--- looprpc/gen_protos.sh
+++ looprpc/gen_protos.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# Generate the protos.
+# Generate the server protos.
  protoc -I/usr/local/include -I.\
            -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
            --go_out=plugins=grpc,paths=source_relative:. \
             server.proto
+
+# Generate the client protos.
+protoc -I/usr/local/include -I. \
+       -I$GOPATH/src \
+       --go_out=plugins=grpc:. \
+       client.proto
