0ef659a486f70408e1a5fe6a4e909e5d894c1a54
Joost Jager <joost.jager@gmail.com>
2020-06-15 11:29:43 +0200

    looprpc: generate with latest lnd versions of the proto generators
    https://github.com/lightningnetwork/lnd/commit/3ef41dd198cf21b3f33f4d844261e94eae67a93d

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/fortytw2/leaktest v1.3.0
 	github.com/golang/protobuf v1.3.2
 	github.com/google/go-cmp v0.3.1 // indirect
-	github.com/grpc-ecosystem/grpc-gateway v1.12.2
+	github.com/grpc-ecosystem/grpc-gateway v1.14.3
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/lightninglabs/protobuf-hex-display v1.3.3-0.20191212020323-b444784ce75d
 	github.com/lightningnetwork/lnd v0.10.0-beta.rc6.0.20200603030653-09bb9db78246
--- looprpc/client.pb.go
+++ looprpc/client.pb.go
@@ -9,6 +9,8 @@ import (
 	proto "github.com/golang/protobuf/proto"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -1561,6 +1563,41 @@ type SwapClientServer interface {
 	//* loop: `listauth`
 	//GetLsatTokens returns all LSAT tokens the daemon ever paid for.
 	GetLsatTokens(context.Context, *TokensRequest) (*TokensResponse, error)
+}
+
+// UnimplementedSwapClientServer can be embedded to have forward compatible implementations.
+type UnimplementedSwapClientServer struct {
+}
+
+func (*UnimplementedSwapClientServer) LoopOut(ctx context.Context, req *LoopOutRequest) (*SwapResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method LoopOut not implemented")
+}
+func (*UnimplementedSwapClientServer) LoopIn(ctx context.Context, req *LoopInRequest) (*SwapResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method LoopIn not implemented")
+}
+func (*UnimplementedSwapClientServer) Monitor(req *MonitorRequest, srv SwapClient_MonitorServer) error {
+	return status.Errorf(codes.Unimplemented, "method Monitor not implemented")
+}
+func (*UnimplementedSwapClientServer) ListSwaps(ctx context.Context, req *ListSwapsRequest) (*ListSwapsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListSwaps not implemented")
+}
+func (*UnimplementedSwapClientServer) SwapInfo(ctx context.Context, req *SwapInfoRequest) (*SwapStatus, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SwapInfo not implemented")
+}
+func (*UnimplementedSwapClientServer) LoopOutTerms(ctx context.Context, req *TermsRequest) (*TermsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method LoopOutTerms not implemented")
+}
+func (*UnimplementedSwapClientServer) LoopOutQuote(ctx context.Context, req *QuoteRequest) (*QuoteResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method LoopOutQuote not implemented")
+}
+func (*UnimplementedSwapClientServer) GetLoopInTerms(ctx context.Context, req *TermsRequest) (*TermsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetLoopInTerms not implemented")
+}
+func (*UnimplementedSwapClientServer) GetLoopInQuote(ctx context.Context, req *QuoteRequest) (*QuoteResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetLoopInQuote not implemented")
+}
+func (*UnimplementedSwapClientServer) GetLsatTokens(ctx context.Context, req *TokensRequest) (*TokensResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetLsatTokens not implemented")
 }
 
 func RegisterSwapClientServer(s *grpc.Server, srv SwapClientServer) {
