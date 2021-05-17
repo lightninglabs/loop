7453a727596e531c850ddb99de27d2fb385ea035
Oliver Gugger <gugger@gmail.com>
2021-05-17 15:38:14 +0200

    version: bump version to v0.13.0-beta

--- version.go
+++ version.go
@@ -26,8 +26,8 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	// Note: please update release_notes.md when you change these values.
 	appMajor uint = 0
-	appMinor uint = 12
-	appPatch uint = 2
+	appMinor uint = 13
+	appPatch uint = 0
 
 	// appPreRelease MUST only contain characters from semanticAlphabet per
 	// the semantic versioning spec.
