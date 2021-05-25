67c81204f6438b86fdc78743e5c0bdeb448bc014
Oliver Gugger <gugger@gmail.com>
2021-05-25 16:57:36 +0200

    looprpc: extract REST annotations into yaml

--- looprpc/gen_protos.sh
+++ looprpc/gen_protos.sh
@@ -6,7 +6,6 @@ set -e
 for file in ./*.proto
 do
 	protoc -I/usr/local/include -I. \
-	       -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
 	       --go_out=plugins=grpc,paths=source_relative:. \
 		${file}
 
@@ -14,11 +13,10 @@ done
 
 # Only generate the REST proxy and definitions for the client component.
 protoc -I/usr/local/include -I. \
-       -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-       --grpc-gateway_out=logtostderr=true:. \
+       --grpc-gateway_out=logtostderr=true,paths=source_relative,grpc_api_configuration=rest-annotations.yaml:. \
        client.proto
 
 protoc -I/usr/local/include -I. \
        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-       --swagger_out=logtostderr=true:. \
+       --swagger_out=logtostderr=true,grpc_api_configuration=rest-annotations.yaml:. \
        client.proto
