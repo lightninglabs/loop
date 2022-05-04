1abbe648d1f7785cb7bb81c72aeaec14ac5a9bf4
Andras Banki-Horvath <bhandras@gmail.com>
2022-05-04 22:25:40 +0200

    rpc: fix swapserverrpc build

--- swapserverrpc/gen_protos.sh
+++ swapserverrpc/gen_protos.sh
@@ -9,6 +9,7 @@ function generate() {
   do
     protoc -I/usr/local/include -I. -I.. \
       --go_out . --go_opt paths=source_relative \
+      --go-grpc_out . --go-grpc_opt paths=source_relative \
       "${file}"
   done
 }
