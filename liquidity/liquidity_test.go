9c5ac0fb36ccee2656e3e7364734fcbed8cb0b16
yyforyongyu <yy2452@columbia.edu>
2022-05-19 01:45:54 +0800

    liquidity: move parameters into a new file
    This commit refactors the `liquidity` by moving `Parameters` related
    code into one file.

--- liquidity/liquidity_test.go
+++ liquidity/liquidity_test.go
@@ -246,60 +246,6 @@ func TestParameters(t *testing.T) {
 	require.Equal(t, ErrZeroChannelID, err)
 }
 
-// TestValidateRestrictions tests validating client restrictions against a set
-// of server restrictions.
-func TestValidateRestrictions(t *testing.T) {
-	tests := []struct {
-		name   string
-		client *Restrictions
-		server *Restrictions
-		err    error
-	}{
-		{
-			name: "client invalid",
-			client: &Restrictions{
-				Minimum: 100,
-				Maximum: 1,
-			},
-			server: testRestrictions,
-			err:    ErrMinimumExceedsMaximumAmt,
-		},
-		{
-			name: "maximum exceeds server",
-			client: &Restrictions{
-				Maximum: 2000,
-			},
-			server: &Restrictions{
-				Minimum: 1000,
-				Maximum: 1500,
-			},
-			err: ErrMaxExceedsServer,
-		},
-		{
-			name: "minimum less than server",
-			client: &Restrictions{
-				Minimum: 500,
-			},
-			server: &Restrictions{
-				Minimum: 1000,
-				Maximum: 1500,
-			},
-			err: ErrMinLessThanServer,
-		},
-	}
-
-	for _, testCase := range tests {
-		testCase := testCase
-
-		t.Run(testCase.name, func(t *testing.T) {
-			err := validateRestrictions(
-				testCase.server, testCase.client,
-			)
-			require.Equal(t, testCase.err, err)
-		})
-	}
-}
-
 // TestRestrictedSuggestions tests getting of swap suggestions when we have
 // other in-flight swaps. We setup our manager with a set of channels and rules
 // that require a loop out swap, focusing on the filtering our of channels that
---
+++ liquidity/parameters_test.go
@@ -0,0 +1,61 @@
+package liquidity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TestValidateRestrictions tests validating client restrictions against a set
+// of server restrictions.
+func TestValidateRestrictions(t *testing.T) {
+	tests := []struct {
+		name   string
+		client *Restrictions
+		server *Restrictions
+		err    error
+	}{
+		{
+			name: "client invalid",
+			client: &Restrictions{
+				Minimum: 100,
+				Maximum: 1,
+			},
+			server: testRestrictions,
+			err:    ErrMinimumExceedsMaximumAmt,
+		},
+		{
+			name: "maximum exceeds server",
+			client: &Restrictions{
+				Maximum: 2000,
+			},
+			server: &Restrictions{
+				Minimum: 1000,
+				Maximum: 1500,
+			},
+			err: ErrMaxExceedsServer,
+		},
+		{
+			name: "minimum less than server",
+			client: &Restrictions{
+				Minimum: 500,
+			},
+			server: &Restrictions{
+				Minimum: 1000,
+				Maximum: 1500,
+			},
+			err: ErrMinLessThanServer,
+		},
+	}
+
+	for _, testCase := range tests {
+		testCase := testCase
+
+		t.Run(testCase.name, func(t *testing.T) {
+			err := validateRestrictions(
+				testCase.server, testCase.client,
+			)
+			require.Equal(t, testCase.err, err)
+		})
+	}
+}
