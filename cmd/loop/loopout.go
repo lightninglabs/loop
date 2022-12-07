f0fa8068a3236de8effa13e725cf3a36c2777e3c
andrew <andrewnijmeh1@gmail.com>
2022-12-07 10:59:40 -0500

    update help

--- cmd/loop/loopout.go
+++ cmd/loop/loopout.go
@@ -41,7 +41,7 @@ var loopOutCommand = cli.Command{
 		},
 		cli.Uint64Flag{
 			Name:  "amt",
-			Usage: "the amount in satoshis to loop out (min: 250k, max: 60m)",
+			Usage: "run the command loop terms to get the minimum & maximum amount on the server. "
 		},
 		cli.Uint64Flag{
 			Name: "htlc_confs",
