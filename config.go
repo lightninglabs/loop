4d558b14185d73b333099e7b42eb7b740b0df816
sputn1ck <kon@kon.ninja>
2023-08-25 01:41:45 +0200

    loop: expose server grpc connection

--- config.go
+++ config.go
@@ -6,12 +6,14 @@ import (
 	"github.com/lightninglabs/aperture/lsat"
 	"github.com/lightninglabs/lndclient"
 	"github.com/lightninglabs/loop/loopdb"
+	"google.golang.org/grpc"
 )
 
 // clientConfig contains config items for the swap client.
 type clientConfig struct {
 	LndServices       *lndclient.LndServices
 	Server            swapServerClient
+	Conn              *grpc.ClientConn
 	Store             loopdb.SwapStore
 	LsatStore         lsat.Store
 	CreateExpiryTimer func(expiry time.Duration) <-chan time.Time
