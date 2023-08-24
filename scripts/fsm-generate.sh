9b178dd9791f508b67d047d71f8314de482309f5
sputn1ck <kon@kon.ninja>
2023-08-25 01:46:25 +0200

    fsm: add reservation fsm compiling

--- scripts/fsm-generate.sh
+++ scripts/fsm-generate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 go run ./fsm/stateparser/stateparser.go --out ./fsm/example_fsm.md --fsm example
+go run ./fsm/stateparser/stateparser.go --out ./reservation/reservation_fsm.md --fsm reservation
