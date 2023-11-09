80fc6e5a51dfa6b8ac52846f3d5f8460c011248e
Slyghtning <hieblmi@protonmail.com>
2023-11-09 19:21:24 +0100

    perms: static address creation permission

--- loopd/perms/perms.go
+++ loopd/perms/perms.go
@@ -69,6 +69,13 @@ var RequiredPermissions = map[string][]bakery.Op{
 		Entity: "loop",
 		Action: "in",
 	}},
+	"/looprpc.StaticAddressClient/NewAddress": {{
+		Entity: "swap",
+		Action: "read",
+	}, {
+		Entity: "loop",
+		Action: "in",
+	}},
 	"/looprpc.SwapClient/GetLsatTokens": {{
 		Entity: "auth",
 		Action: "read",
