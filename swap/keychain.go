fdf77ed56d3f96d9b14186721a9ae16d8f321e0d
Slyghtning <hieblmi@protonmail.com>
2024-01-31 11:03:41 +0100

    swap: address family for static addresses

--- swap/keychain.go
+++ swap/keychain.go
@@ -4,4 +4,8 @@ var (
 	// KeyFamily is the key family used to generate keys that allow
 	// spending of the htlc.
 	KeyFamily = int32(99)
+
+	// StaticAddressKeyFamily is the key family used to generate static
+	// address keys.
+	StaticAddressKeyFamily = int32(42060)
 )
