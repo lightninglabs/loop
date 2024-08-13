8b7f1a7f0b0047d780b540714e99cf566c0cc5a0
sputn1ck <kon@kon.ninja>
2024-08-13 12:36:28 +0200

    fsm: add hyperloop fsm

--- scripts/fsm-generate.sh
+++ scripts/fsm-generate.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 go run ./fsm/stateparser/stateparser.go --out ./fsm/example_fsm.md --fsm example
-go run ./fsm/stateparser/stateparser.go --out ./reservation/reservation_fsm.md --fsm reservation
+go run ./fsm/stateparser/stateparser.go --out ./instantout/reservation/reservation_fsm.md --fsm reservation
 go run ./fsm/stateparser/stateparser.go --out ./instantout/fsm.md --fsm instantout
+go run ./fsm/stateparser/stateparser.go --out ./hyperloop/fsm.md --fsm hyperloop
