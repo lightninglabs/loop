d7913ae041fee20544b57a0b2c0055daf33687c8
Slyghtning <hieblmi@protonmail.com>
2024-08-28 10:21:57 +0200

    staticaddr: withdrawal changes to be squashed

--- loopdb/sqlc/models.go
+++ loopdb/sqlc/models.go
@@ -10,15 +10,15 @@ import (
 )
 
 type Deposit struct {
-	ID                     int32
-	DepositID              []byte
-	TxHash                 []byte
-	OutIndex               int32
-	Amount                 int64
-	ConfirmationHeight     int64
-	TimeoutSweepPkScript   []byte
-	ExpirySweepTxid        []byte
-	WithdrawalSweepAddress sql.NullString
+	ID                    int32
+	DepositID             []byte
+	TxHash                []byte
+	OutIndex              int32
+	Amount                int64
+	ConfirmationHeight    int64
+	TimeoutSweepPkScript  []byte
+	ExpirySweepTxid       []byte
+	FinalizedWithdrawalTx sql.NullString
 }
 
 type DepositUpdate struct {
