b9e5875f4e5c5e6d5687b719ec11eb74d799746a
Slyghtning <hieblmi@protonmail.com>
2024-11-19 13:15:53 +0100

    looprpc: static address loop-ins

--- looprpc/swapclient.pb.json.go
+++ looprpc/swapclient.pb.json.go
@@ -687,4 +687,29 @@ func RegisterSwapClientJSONCallbacks(registry map[string]func(ctx context.Contex
 		}
 		callback(string(respBytes), nil)
 	}
+
+	registry["looprpc.SwapClient.StaticAddressLoopIn"] = func(ctx context.Context,
+		conn *grpc.ClientConn, reqJSON string, callback func(string, error)) {
+
+		req := &StaticAddressLoopInRequest{}
+		err := marshaler.Unmarshal([]byte(reqJSON), req)
+		if err != nil {
+			callback("", err)
+			return
+		}
+
+		client := NewSwapClientClient(conn)
+		resp, err := client.StaticAddressLoopIn(ctx, req)
+		if err != nil {
+			callback("", err)
+			return
+		}
+
+		respBytes, err := marshaler.Marshal(resp)
+		if err != nil {
+			callback("", err)
+			return
+		}
+		callback(string(respBytes), nil)
+	}
 }
