b78cb2fd746b2fb753da0885e632d796c4cddfb0
sputn1ck <kon@kon.ninja>
2025-02-03 14:00:10 +0100

    reservation: add protocol version

--- instantout/reservation/reservation.go
+++ instantout/reservation/reservation.go
@@ -37,6 +37,10 @@ type Reservation struct {
 	// ID is the unique identifier of the reservation.
 	ID ID
 
+	// ProtocolVersion is the version of the protocol used for the
+	// reservation.
+	ProtocolVersion ProtocolVersion
+
 	// State is the current state of the reservation.
 	State fsm.StateType
 
@@ -69,8 +73,8 @@ type Reservation struct {
 
 func NewReservation(id ID, serverPubkey, clientPubkey *btcec.PublicKey,
 	value btcutil.Amount, expiry, heightHint uint32,
-	keyLocator keychain.KeyLocator) (*Reservation,
-	error) {
+	keyLocator keychain.KeyLocator, protocolVersion ProtocolVersion) (
+	*Reservation, error) {
 
 	if id == [32]byte{} {
 		return nil, errors.New("id is empty")
@@ -103,6 +107,7 @@ func NewReservation(id ID, serverPubkey, clientPubkey *btcec.PublicKey,
 		KeyLocator:       keyLocator,
 		Expiry:           expiry,
 		InitiationHeight: int32(heightHint),
+		ProtocolVersion:  protocolVersion,
 	}, nil
 }
 
