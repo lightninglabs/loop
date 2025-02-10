b93c2552638c1ab7e710afac07d5a47bb4755e7b
Boris Nagaev <bnagaev@gmail.com>
2025-02-10 18:39:17 -0300

    sweepbatcher: add mode with presigned transactions
    In this mode sweepbatcher uses transactions provided by the Presigned helper.
    Transactions are signed upon adding an input to a batch.
    
    A single Batcher instance can handle both presigned and regular batches.
    Currently presigned and non-presigned sweeps never appear in the same batch.

---
+++ sweepbatcher/presigned_test.go
@@ -0,0 +1,848 @@
+package sweepbatcher
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
+	"github.com/stretchr/testify/require"
+)
+
+// mockPresigner is an implementation of Presigner used in TestPresign.
+type mockPresigner struct {
+	// outputs collects outputs of presigned transactions.
+	outputs []btcutil.Amount
+
+	// lockTimes collects LockTime's of presigned transactions.
+	lockTimes []uint32
+
+	// failAt is optional index of a call at which it fails, 1 based.
+	failAt int
+}
+
+// Presign memorizes the value of the output and fails if the number of
+// calls previously made is failAt.
+func (p *mockPresigner) Presign(ctx context.Context, tx *wire.MsgTx,
+	inputAmt btcutil.Amount) error {
+
+	if len(p.outputs)+1 == p.failAt {
+		return fmt.Errorf("test error in Presign")
+	}
+
+	p.outputs = append(p.outputs, btcutil.Amount(tx.TxOut[0].Value))
+	p.lockTimes = append(p.lockTimes, tx.LockTime)
+
+	return nil
+}
+
+// TestPresign checks that function presign presigns correct set of transactions
+// and handles edge cases properly.
+func TestPresign(t *testing.T) {
+	// Prepare the necessary data for test cases.
+	op1 := wire.OutPoint{
+		Hash:  chainhash.Hash{1, 1, 1},
+		Index: 1,
+	}
+	op2 := wire.OutPoint{
+		Hash:  chainhash.Hash{2, 2, 2},
+		Index: 2,
+	}
+
+	ctx := context.Background()
+
+	cases := []struct {
+		name             string
+		presigner        presigner
+		sweeps           []sweep
+		destAddr         btcutil.Address
+		nextBlockFeerate chainfee.SatPerKWeight
+		wantErr          string
+		wantOutputs      []btcutil.Amount
+		wantLockTimes    []uint32
+	}{
+		{
+			name: "error: no presigner",
+			sweeps: []sweep{
+				{
+					outpoint: op1,
+					value:    1_000_000,
+					timeout:  1000,
+				},
+			},
+			destAddr:         destAddr,
+			nextBlockFeerate: chainfee.FeePerKwFloor,
+			wantErr:          "presigner is not installed",
+		},
+
+		{
+			name:             "error: no sweeps",
+			presigner:        &mockPresigner{},
+			destAddr:         destAddr,
+			nextBlockFeerate: chainfee.FeePerKwFloor,
+			wantErr:          "there are no sweeps",
+		},
+
+		{
+			name:      "error: no destAddr",
+			presigner: &mockPresigner{},
+			sweeps: []sweep{
+				{
+					outpoint: op1,
+					value:    1_000_000,
+					timeout:  1000,
+				},
+			},
+			nextBlockFeerate: chainfee.FeePerKwFloor,
+			wantErr:          "unsupported address type <nil>",
+		},
+
+		{
+			name:      "error: zero nextBlockFeerate",
+			presigner: &mockPresigner{},
+			sweeps: []sweep{
+				{
+					outpoint: op1,
+					value:    1_000_000,
+					timeout:  1000,
+				},
+				{
+					outpoint: op2,
+					value:    2_000_000,
+					timeout:  1000,
+				},
+			},
+			destAddr: destAddr,
+			wantErr:  "nextBlockFeerate is not set",
+		},
+
+		{
+			name:      "error: timeout is not set",
+			presigner: &mockPresigner{},
+			sweeps: []sweep{
+				{
+					outpoint: op1,
+					value:    1_000_000,
+				},
+				{
+					outpoint: op2,
+					value:    2_000_000,
+				},
+			},
+			destAddr:         destAddr,
+			nextBlockFeerate: chainfee.FeePerKwFloor,
+			wantErr:          "timeout is invalid: 0",
+		},
+
+		{
+			name:      "two coop sweeps",
+			presigner: &mockPresigner{},
+			sweeps: []sweep{
+				{
+					outpoint: op1,
+					value:    1_000_000,
+					timeout:  1000,
+				},
+				{
+					outpoint: op2,
+					value:    2_000_000,
+					timeout:  1100,
+				},
+			},
+			destAddr:         destAddr,
+			nextBlockFeerate: chainfee.FeePerKwFloor,
+			wantOutputs: []btcutil.Amount{
+				2999842, 2999811, 2999773, 2999728, 2999674,
+				2999609, 2999530, 2999436, 2999324, 2999189,
+				2999026, 2998832, 2998598, 2998318, 2997982,
+				2997578, 2997093, 2996512, 2995815, 2994978,
+				2993974, 2992769, 2991323, 2989588, 2987506,
+				2985007, 2982008, 2978410, 2974092, 2968910,
+				2962692, 2955231, 2946277, 2935533, 2922639,
+				2907167, 2888601, 2866321, 2839585, 2807502,
+				2769002, 2722803,
+			},
+			wantLockTimes: []uint32{
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 950, 950, 950,
+				950, 950, 950, 950, 950, 950, 950, 950, 950,
+				950, 950, 950, 950,
+			},
+		},
+
+		{
+			name:      "high current feerate => locktime later",
+			presigner: &mockPresigner{},
+			sweeps: []sweep{
+				{
+					outpoint: op1,
+					value:    1_000_000,
+					timeout:  1000,
+				},
+				{
+					outpoint: op2,
+					value:    2_000_000,
+					timeout:  1000,
+				},
+			},
+			destAddr:         destAddr,
+			nextBlockFeerate: 50 * chainfee.FeePerKwFloor,
+			wantOutputs: []btcutil.Amount{
+				2999842, 2999811, 2999773, 2999728, 2999674,
+				2999609, 2999530, 2999436, 2999324, 2999189,
+				2999026, 2998832, 2998598, 2998318, 2997982,
+				2997578, 2997093, 2996512, 2995815, 2994978,
+				2993974, 2992769, 2991323, 2989588, 2987506,
+				2985007, 2982008, 2978410, 2974092, 2968910,
+				2962692, 2955231, 2946277, 2935533, 2922639,
+				2907167, 2888601, 2866321, 2839585, 2807502,
+				2769002, 2722803,
+			},
+			wantLockTimes: []uint32{
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 950, 950, 950, 950, 950, 950, 950,
+			},
+		},
+
+		{
+			name:      "small amount => fewer steps until clamped",
+			presigner: &mockPresigner{},
+			sweeps: []sweep{
+				{
+					outpoint: op1,
+					value:    1_000,
+					timeout:  1000,
+				},
+				{
+					outpoint: op2,
+					value:    2_000,
+					timeout:  1000,
+				},
+			},
+			destAddr:         destAddr,
+			nextBlockFeerate: chainfee.FeePerKwFloor,
+			wantOutputs: []btcutil.Amount{
+				2842, 2811, 2773, 2728, 2674, 2609, 2530, 2436,
+				2400,
+			},
+			wantLockTimes: []uint32{
+				0, 0, 0, 0, 0, 0, 0, 0, 0,
+			},
+		},
+
+		{
+			name: "third signing fails",
+			presigner: &mockPresigner{
+				failAt: 3,
+			},
+			sweeps: []sweep{
+				{
+					outpoint: op1,
+					value:    1_000,
+					timeout:  1000,
+				},
+				{
+					outpoint: op2,
+					value:    2_000,
+					timeout:  1000,
+				},
+			},
+			destAddr:         destAddr,
+			nextBlockFeerate: chainfee.FeePerKwFloor,
+			wantErr:          "for feeRate 363 sat/kw",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			err := presign(
+				ctx, tc.presigner, tc.destAddr, tc.sweeps,
+				tc.nextBlockFeerate,
+			)
+			if tc.wantErr != "" {
+				require.Error(t, err)
+				require.ErrorContains(t, err, tc.wantErr)
+			} else {
+				require.NoError(t, err)
+				p := tc.presigner.(*mockPresigner)
+				require.Equal(t, tc.wantOutputs, p.outputs)
+				require.Equal(t, tc.wantLockTimes, p.lockTimes)
+			}
+		})
+	}
+}
+
+// TestCheckSignedTx tests that function CheckSignedTx checks all the criteria
+// of PresignedHelper.SignTx correctly.
+func TestCheckSignedTx(t *testing.T) {
+	// Prepare the necessary data for test cases.
+	op1 := wire.OutPoint{
+		Hash:  chainhash.Hash{1, 1, 1},
+		Index: 1,
+	}
+	op2 := wire.OutPoint{
+		Hash:  chainhash.Hash{2, 2, 2},
+		Index: 2,
+	}
+
+	batchPkScript, err := txscript.PayToAddrScript(destAddr)
+	require.NoError(t, err)
+
+	cases := []struct {
+		name        string
+		unsignedTx  *wire.MsgTx
+		signedTx    *wire.MsgTx
+		inputAmt    btcutil.Amount
+		minRelayFee chainfee.SatPerKWeight
+		wantErr     string
+	}{
+		{
+			name: "success",
+			unsignedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+					},
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 800_000,
+			},
+			signedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 799_999,
+			},
+			inputAmt:    3_000_000,
+			minRelayFee: 253,
+			wantErr:     "",
+		},
+
+		{
+			name: "bad locktime",
+			unsignedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+					},
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 800_000,
+			},
+			signedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 800_001,
+			},
+			inputAmt:    3_000_000,
+			minRelayFee: 253,
+			wantErr:     "locktime",
+		},
+
+		{
+			name: "bad version",
+			unsignedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+					},
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 800_000,
+			},
+			signedTx: &wire.MsgTx{
+				Version: 3,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 799_999,
+			},
+			inputAmt:    3_000_000,
+			minRelayFee: 253,
+			wantErr:     "version",
+		},
+
+		{
+			name: "missing input",
+			unsignedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+					},
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 800_000,
+			},
+			signedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 799_999,
+			},
+			inputAmt:    3_000_000,
+			minRelayFee: 253,
+			wantErr:     "is missing in signed tx",
+		},
+
+		{
+			name: "extra input",
+			unsignedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 800_000,
+			},
+			signedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 799_999,
+			},
+			inputAmt:    3_000_000,
+			minRelayFee: 253,
+			wantErr:     "is new in signed tx",
+		},
+
+		{
+			name: "mismatch of sequence numbers",
+			unsignedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+					},
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 800_000,
+			},
+			signedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+					{
+						PreviousOutPoint: op1,
+						Sequence:         3,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 799_999,
+			},
+			inputAmt:    3_000_000,
+			minRelayFee: 253,
+			wantErr:     "sequence mismatch",
+		},
+
+		{
+			name: "extra output in unsignedTx",
+			unsignedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+					},
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 800_000,
+			},
+			signedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 799_999,
+			},
+			inputAmt:    3_000_000,
+			minRelayFee: 253,
+			wantErr:     "unsigned tx has 2 outputs, want 1",
+		},
+
+		{
+			name: "extra output in signedTx",
+			unsignedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+					},
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 800_000,
+			},
+			signedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 799_999,
+			},
+			inputAmt:    3_000_000,
+			minRelayFee: 253,
+			wantErr:     "the signed tx has 2 outputs, want 1",
+		},
+
+		{
+			name: "mismatch of output pk_script",
+			unsignedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+					},
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 800_000,
+			},
+			signedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript[1:],
+					},
+				},
+				LockTime: 799_999,
+			},
+			inputAmt:    3_000_000,
+			minRelayFee: 253,
+			wantErr:     "mismatch of output pkScript",
+		},
+
+		{
+			name: "too low feerate in signedTx",
+			unsignedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+					},
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 800_000,
+			},
+			signedTx: &wire.MsgTx{
+				Version: 2,
+				TxIn: []*wire.TxIn{
+					{
+						PreviousOutPoint: op2,
+						Sequence:         2,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+					{
+						PreviousOutPoint: op1,
+						Sequence:         1,
+						Witness: wire.TxWitness{
+							[]byte("test"),
+						},
+					},
+				},
+				TxOut: []*wire.TxOut{
+					{
+						Value:    2999374,
+						PkScript: batchPkScript,
+					},
+				},
+				LockTime: 799_999,
+			},
+			inputAmt:    3_000_000,
+			minRelayFee: 250_000,
+			wantErr:     "is lower than minRelayFee",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			err := CheckSignedTx(
+				tc.unsignedTx, tc.signedTx, tc.inputAmt,
+				tc.minRelayFee,
+			)
+			if tc.wantErr != "" {
+				require.Error(t, err)
+				require.ErrorContains(t, err, tc.wantErr)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
--- sweepbatcher/sweep_batcher.go
+++ sweepbatcher/sweep_batcher.go
@@ -153,6 +153,49 @@ type SignMuSig2 func(ctx context.Context, muSig2Version input.MuSig2Version,
 	swapHash lntypes.Hash, rootHash chainhash.Hash, sigHash [32]byte,
 ) ([]byte, error)
 
+// PresignedHelper provides methods used when batches are presigned in advance.
+// In this mode sweepbatcher uses transactions provided by PresignedHelper,
+// which are pre-signed. The helper also memorizes transactions it previously
+// produced. It also affects batch selection: presigned inputs and regular
+// (non-presigned) inputs never appear in the same batch. Also if presigning
+// fails (e.g. because one of the inputs is offline), an input can't be added to
+// a batch.
+type PresignedHelper interface {
+	// IsPresigned returns if presigned mode is enabled for a particular
+	// sweep. This method should always return the same value for the same
+	// sweep. Currently presigned and non-presigned sweeps never appear in
+	// the same batch.
+	IsPresigned(ctx context.Context, input wire.OutPoint) (bool, error)
+
+	// Presign tries to presign a batch transaction. If the method returns
+	// nil, it is guaranteed that future calls to SignTx on this set of
+	// sweeps return valid signed transactions.
+	Presign(ctx context.Context, tx *wire.MsgTx,
+		inputAmt btcutil.Amount) error
+
+	// DestPkScript returns destination pkScript used in a presigned
+	// transaction sweeping the inputs. Returns an error, if such tx
+	// doesn't exist. If there are many such transactions, returns any
+	// of pkScript's.
+	DestPkScript(ctx context.Context,
+		inputs []wire.OutPoint) ([]byte, error)
+
+	// SignTx signs an unsigned transaction or returns a pre-signed tx.
+	// It must satisfy the following invariants:
+	//   - the set of inputs is the same, though the order may change;
+	//   - the output is the same, but its amount may be different;
+	//   - feerate is higher or equal to minRelayFee;
+	//   - LockTime may be decreased;
+	//   - transaction version must be the same;
+	//   - Sequence numbers in the inputs must be preserved.
+	SignTx(ctx context.Context, tx *wire.MsgTx, inputAmt btcutil.Amount,
+		minRelayFee chainfee.SatPerKWeight) (*wire.MsgTx, error)
+
+	// CleanupTransactions removes all transactions related to any of the
+	// outpoints. Should be called after sweep batch tx is fully confirmed.
+	CleanupTransactions(ctx context.Context, inputs []wire.OutPoint) error
+}
+
 // VerifySchnorrSig is a function that can be used to verify a schnorr
 // signature.
 type VerifySchnorrSig func(pubKey *btcec.PublicKey, hash, sig []byte) error
@@ -329,6 +372,10 @@ type Batcher struct {
 	// error. By default, it logs all errors as warnings, but "insufficient
 	// fee" as Info.
 	publishErrorHandler PublishErrorHandler
+
+	// presignedHelper provides methods used when presigned batches are
+	// enabled.
+	presignedHelper PresignedHelper
 }
 
 // BatcherConfig holds batcher configuration.
@@ -369,6 +416,10 @@ type BatcherConfig struct {
 	// error. By default, it logs all errors as warnings, but "insufficient
 	// fee" as Info.
 	publishErrorHandler PublishErrorHandler
+
+	// presignedHelper provides methods used when presigned batches are
+	// enabled.
+	presignedHelper PresignedHelper
 }
 
 // BatcherOption configures batcher behaviour.
@@ -442,6 +493,15 @@ func WithPublishErrorHandler(handler PublishErrorHandler) BatcherOption {
 	}
 }
 
+// WithPresignedHelper enables presigned batches in the batcher. When a sweep
+// intended for presigning is added, it must be first passed to the PresignSweep
+// method, before first call of the AddSweep method.
+func WithPresignedHelper(presignedHelper PresignedHelper) BatcherOption {
+	return func(cfg *BatcherConfig) {
+		cfg.presignedHelper = presignedHelper
+	}
+}
+
 // NewBatcher creates a new Batcher instance.
 func NewBatcher(wallet lndclient.WalletKitClient,
 	chainNotifier lndclient.ChainNotifierClient,
@@ -496,6 +556,7 @@ func NewBatcher(wallet lndclient.WalletKitClient,
 		txLabeler:           cfg.txLabeler,
 		customMuSig2Signer:  cfg.customMuSig2Signer,
 		publishErrorHandler: cfg.publishErrorHandler,
+		presignedHelper:     cfg.presignedHelper,
 	}
 }
 
@@ -564,8 +625,40 @@ func (b *Batcher) Run(ctx context.Context) error {
 	}
 }
 
+// PresignSweep creates and stores presigned 1:1 transactions for the sweep.
+// This method must be called prior to AddSweep if presigned mode is enabled.
+func (b *Batcher) PresignSweep(ctx context.Context, sweepOutpoint wire.OutPoint,
+	sweepValue btcutil.Amount, sweepTimeout int32,
+	destAddress btcutil.Address) error {
+
+	if b.presignedHelper == nil {
+		return fmt.Errorf("presignedHelper is not installed")
+	}
+
+	// Find the feerate needed to get into next block. Use conf_target=2,
+	nextBlockFeerate, err := b.wallet.EstimateFeeRate(ctx, 2)
+	if err != nil {
+		return fmt.Errorf("failed to get nextBlockFeerate: %w", err)
+	}
+	log().Infof("nextBlockFeerate is %v", nextBlockFeerate)
+
+	sweeps := []sweep{
+		{
+			outpoint: sweepOutpoint,
+			value:    sweepValue,
+			timeout:  sweepTimeout,
+		},
+	}
+
+	return presign(
+		ctx, b.presignedHelper, destAddress, sweeps, nextBlockFeerate,
+	)
+}
+
 // AddSweep adds a sweep request to the batcher for handling. This will either
-// place the sweep in an existing batch or create a new one.
+// place the sweep in an existing batch or create a new one. In presigned mode
+// call PresignSweep prior to AddSweep. If PresignSweep fails, AddSweep must not
+// be called.
 func (b *Batcher) AddSweep(sweepReq *SweepRequest) error {
 	select {
 	case b.sweepReqs <- *sweepReq:
@@ -616,8 +709,8 @@ func (b *Batcher) handleSweep(ctx context.Context, sweep *sweep,
 		return err
 	}
 
-	log().Infof("Batcher handling sweep %x, completed=%v",
-		sweep.swapHash[:6], completed)
+	log().Infof("Batcher handling sweep %x, presigned=%v, completed=%v",
+		sweep.swapHash[:6], sweep.presigned, completed)
 
 	// If the sweep has already been completed in a confirmed batch then we
 	// can't attach its notifier to the batch as that is no longer running.
@@ -703,7 +796,9 @@ func (b *Batcher) handleSweep(ctx context.Context, sweep *sweep,
 	return b.spinUpNewBatch(ctx, sweep)
 }
 
-// spinUpNewBatch creates new batch, starts it and adds the sweep to it.
+// spinUpNewBatch creates new batch, starts it and adds the sweep to it. If
+// presigned mode is enabled, the result also depends on outcome of
+// presignedHelper.Presign.
 func (b *Batcher) spinUpNewBatch(ctx context.Context, sweep *sweep) error {
 	// Spin up a fresh batch.
 	newBatch, err := b.spinUpBatch(ctx)
@@ -1099,6 +1194,16 @@ func (b *Batcher) loadSweep(ctx context.Context, swapHash lntypes.Hash,
 			swapHash[:6], err)
 	}
 
+	// Determine if presigned mode is used for this sweep.
+	var presigned bool
+	if b.presignedHelper != nil {
+		presigned, err = b.presignedHelper.IsPresigned(ctx, outpoint)
+		if err != nil {
+			return nil, fmt.Errorf("failed to determine presigned "+
+				"status for sweep %x: %w", swapHash[:6], err)
+		}
+	}
+
 	// Find minimum fee rate for the sweep. Use customFeeRate if it is
 	// provided, otherwise use wallet's EstimateFeeRate.
 	var minFeeRate chainfee.SatPerKWeight
@@ -1142,6 +1247,7 @@ func (b *Batcher) loadSweep(ctx context.Context, swapHash lntypes.Hash,
 		destAddr:               s.DestAddr,
 		minFeeRate:             minFeeRate,
 		nonCoopHint:            s.NonCoopHint,
+		presigned:              presigned,
 	}, nil
 }
 
@@ -1152,7 +1258,9 @@ func (b *Batcher) newBatchConfig(maxTimeoutDistance int32) batchConfig {
 		noBumping:          b.customFeeRate != nil,
 		txLabeler:          b.txLabeler,
 		customMuSig2Signer: b.customMuSig2Signer,
+		presignedHelper:    b.presignedHelper,
 		clock:              b.clock,
+		chainParams:        b.chainParams,
 	}
 }
 
