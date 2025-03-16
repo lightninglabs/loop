59b5dff56558fd1cb1e82cfab345090a3215546b
sputn1ck <kon@kon.ninja>
2025-03-15 21:22:56 -0500

    assets: update assets client

--- assets/client.go
+++ assets/client.go
@@ -8,20 +8,43 @@ import (
 	"sync"
 	"time"
 
+	"bytes"
+
 	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/btcutil/psbt"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
+	tap "github.com/lightninglabs/taproot-assets"
+	"github.com/lightninglabs/taproot-assets/asset"
+	"github.com/lightninglabs/taproot-assets/proof"
 	"github.com/lightninglabs/taproot-assets/rfqmath"
 	"github.com/lightninglabs/taproot-assets/rpcutils"
 	"github.com/lightninglabs/taproot-assets/tapcfg"
+	"github.com/lightninglabs/taproot-assets/tappsbt"
 	"github.com/lightninglabs/taproot-assets/taprpc"
+	"github.com/lightninglabs/taproot-assets/taprpc/assetwalletrpc"
+	wrpc "github.com/lightninglabs/taproot-assets/taprpc/assetwalletrpc"
+	"github.com/lightninglabs/taproot-assets/taprpc/mintrpc"
 	"github.com/lightninglabs/taproot-assets/taprpc/priceoraclerpc"
 	"github.com/lightninglabs/taproot-assets/taprpc/rfqrpc"
 	"github.com/lightninglabs/taproot-assets/taprpc/tapchannelrpc"
+	"github.com/lightninglabs/taproot-assets/taprpc/tapdevrpc"
 	"github.com/lightninglabs/taproot-assets/taprpc/universerpc"
+	"github.com/lightninglabs/taproot-assets/tapsend"
+	"github.com/lightninglabs/taproot-assets/universe"
+	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lnrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/routerrpc"
+	"github.com/lightningnetwork/lnd/lntypes"
+	"github.com/lightningnetwork/lnd/lnwallet/btcwallet"
+	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/macaroons"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
 	"gopkg.in/macaroon.v2"
 )
 
@@ -65,7 +88,10 @@ type TapdClient struct {
 	tapchannelrpc.TaprootAssetChannelsClient
 	priceoraclerpc.PriceOracleClient
 	rfqrpc.RfqClient
+	wrpc.AssetWalletClient
+	mintrpc.MintClient
 	universerpc.UniverseClient
+	tapdevrpc.TapDevClient
 
 	cfg            *TapdConfig
 	assetNameCache map[string]string
@@ -312,4 +338,500 @@ func getClientConn(config *TapdConfig) (*grpc.ClientConn, error) {
 	}
 
 	return conn, nil
+}
+
+// FundAndSignVpacket funds and signs a vpacket.
+func (t *TapdClient) FundAndSignVpacket(ctx context.Context,
+	vpkt *tappsbt.VPacket) (*tappsbt.VPacket, error) {
+
+	// Fund the packet.
+	var buf bytes.Buffer
+	err := vpkt.Serialize(&buf)
+	if err != nil {
+		return nil, err
+	}
+
+	fundResp, err := t.FundVirtualPsbt(
+		ctx, &assetwalletrpc.FundVirtualPsbtRequest{
+			Template: &assetwalletrpc.FundVirtualPsbtRequest_Psbt{
+				Psbt: buf.Bytes(),
+			},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// Sign the packet.
+	signResp, err := t.SignVirtualPsbt(
+		ctx, &assetwalletrpc.SignVirtualPsbtRequest{
+			FundedPsbt: fundResp.FundedPsbt,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return tappsbt.NewFromRawBytes(
+		bytes.NewReader(signResp.SignedPsbt), false,
+	)
+}
+
+// addP2WPKHOutputToPsbt adds a normal bitcoin P2WPKH output to a psbt for the
+// given key and amount.
+func addP2WPKHOutputToPsbt(packet *psbt.Packet, keyDesc keychain.KeyDescriptor,
+	amount btcutil.Amount, params *chaincfg.Params) error {
+
+	derivation, _, _ := btcwallet.Bip32DerivationFromKeyDesc(
+		keyDesc, params.HDCoinType,
+	)
+
+	// Convert to Bitcoin address.
+	pubKeyBytes := keyDesc.PubKey.SerializeCompressed()
+	pubKeyHash := btcutil.Hash160(pubKeyBytes)
+	address, err := btcutil.NewAddressWitnessPubKeyHash(pubKeyHash, params)
+	if err != nil {
+		return err
+	}
+
+	// Generate the P2WPKH scriptPubKey.
+	scriptPubKey, err := txscript.PayToAddrScript(address)
+	if err != nil {
+		return err
+	}
+
+	// Add the output to the packet.
+	packet.UnsignedTx.AddTxOut(
+		wire.NewTxOut(int64(amount), scriptPubKey),
+	)
+
+	packet.Outputs = append(packet.Outputs, psbt.POutput{
+		Bip32Derivation: []*psbt.Bip32Derivation{
+			derivation,
+		},
+	})
+
+	return nil
+}
+
+// PrepareAndCommitVirtualPsbts prepares and commits virtual psbt to a BTC
+// template so that the underlying wallet can fund the transaction and add
+// the necessary additional input to pay for fees as well as a change output
+// if the change keydescriptor is not provided.
+func (t *TapdClient) PrepareAndCommitVirtualPsbts(ctx context.Context,
+	vpkt *tappsbt.VPacket, feeRateSatPerVByte chainfee.SatPerVByte,
+	changeKeyDesc *keychain.KeyDescriptor, params *chaincfg.Params) (
+	*psbt.Packet, []*tappsbt.VPacket, []*tappsbt.VPacket,
+	*assetwalletrpc.CommitVirtualPsbtsResponse, error) {
+
+	encodedVpkt, err := tappsbt.Encode(vpkt)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	btcPkt, err := tapsend.PrepareAnchoringTemplate(
+		[]*tappsbt.VPacket{vpkt},
+	)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	commitRequest := &assetwalletrpc.CommitVirtualPsbtsRequest{
+		Fees: &assetwalletrpc.CommitVirtualPsbtsRequest_SatPerVbyte{
+			SatPerVbyte: uint64(feeRateSatPerVByte),
+		},
+		AnchorChangeOutput: &assetwalletrpc.CommitVirtualPsbtsRequest_Add{ //nolint:lll
+			Add: true,
+		},
+		VirtualPsbts: [][]byte{
+			encodedVpkt,
+		},
+	}
+	if changeKeyDesc != nil {
+		err = addP2WPKHOutputToPsbt(
+			btcPkt, *changeKeyDesc, btcutil.Amount(1), params,
+		)
+		if err != nil {
+			return nil, nil, nil, nil, err
+		}
+		commitRequest.AnchorChangeOutput =
+			&assetwalletrpc.CommitVirtualPsbtsRequest_ExistingOutputIndex{ //nolint:lll
+				ExistingOutputIndex: 1,
+			}
+	} else {
+		commitRequest.AnchorChangeOutput =
+			&assetwalletrpc.CommitVirtualPsbtsRequest_Add{
+				Add: true,
+			}
+	}
+	var buf bytes.Buffer
+	err = btcPkt.Serialize(&buf)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	commitRequest.AnchorPsbt = buf.Bytes()
+
+	commitResponse, err := t.AssetWalletClient.CommitVirtualPsbts(
+		ctx, commitRequest,
+	)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	fundedPacket, err := psbt.NewFromRawBytes(
+		bytes.NewReader(commitResponse.AnchorPsbt), false,
+	)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	activePackets := make(
+		[]*tappsbt.VPacket, len(commitResponse.VirtualPsbts),
+	)
+	for idx := range commitResponse.VirtualPsbts {
+		activePackets[idx], err = tappsbt.Decode(
+			commitResponse.VirtualPsbts[idx],
+		)
+		if err != nil {
+			return nil, nil, nil, nil, err
+		}
+	}
+
+	passivePackets := make(
+		[]*tappsbt.VPacket, len(commitResponse.PassiveAssetPsbts),
+	)
+	for idx := range commitResponse.PassiveAssetPsbts {
+		passivePackets[idx], err = tappsbt.Decode(
+			commitResponse.PassiveAssetPsbts[idx],
+		)
+		if err != nil {
+			return nil, nil, nil, nil, err
+		}
+	}
+
+	return fundedPacket, activePackets, passivePackets, commitResponse, nil
+}
+
+// LogAndPublish logs and publishes a psbt with the given active and passive
+// assets.
+func (t *TapdClient) LogAndPublish(ctx context.Context, btcPkt *psbt.Packet,
+	activeAssets []*tappsbt.VPacket, passiveAssets []*tappsbt.VPacket,
+	commitResp *assetwalletrpc.CommitVirtualPsbtsResponse) (
+	*taprpc.SendAssetResponse, error) {
+
+	var buf bytes.Buffer
+	err := btcPkt.Serialize(&buf)
+	if err != nil {
+		return nil, err
+	}
+
+	request := &assetwalletrpc.PublishAndLogRequest{
+		AnchorPsbt:        buf.Bytes(),
+		VirtualPsbts:      make([][]byte, len(activeAssets)),
+		PassiveAssetPsbts: make([][]byte, len(passiveAssets)),
+		ChangeOutputIndex: commitResp.ChangeOutputIndex,
+		LndLockedUtxos:    commitResp.LndLockedUtxos,
+	}
+
+	for idx := range activeAssets {
+		request.VirtualPsbts[idx], err = tappsbt.Encode(
+			activeAssets[idx],
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+	for idx := range passiveAssets {
+		request.PassiveAssetPsbts[idx], err = tappsbt.Encode(
+			passiveAssets[idx],
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	resp, err := t.PublishAndLogTransfer(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// ListAvailableAssets returns a list of available assets.
+func (t *TapdClient) ListAvailableAssets(ctx context.Context) (
+	[][]byte, error) {
+
+	balanceRes, err := t.ListBalances(ctx, &taprpc.ListBalancesRequest{
+		GroupBy: &taprpc.ListBalancesRequest_AssetId{
+			AssetId: true,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	assets := make([][]byte, 0, len(balanceRes.AssetBalances))
+	for assetID := range balanceRes.AssetBalances {
+		asset, err := hex.DecodeString(assetID)
+		if err != nil {
+			return nil, err
+		}
+		assets = append(assets, asset)
+	}
+
+	return assets, nil
+}
+
+// GetAssetBalance checks the balance of an asset by its ID.
+func (t *TapdClient) GetAssetBalance(ctx context.Context, assetId []byte) (
+	uint64, error) {
+
+	// Check if we have enough funds to do the swap.
+	balanceResp, err := t.ListBalances(
+		ctx, &taprpc.ListBalancesRequest{
+			GroupBy: &taprpc.ListBalancesRequest_AssetId{
+				AssetId: true,
+			},
+			AssetFilter: assetId,
+		},
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	// Check if we have enough funds to do the swap.
+	balance, ok := balanceResp.AssetBalances[hex.EncodeToString(
+		assetId,
+	)]
+	if !ok {
+		return 0, status.Error(codes.Internal, "internal error")
+	}
+
+	return balance.Balance, nil
+}
+
+// GetUnEncumberedAssetBalance returns the total balance of the given asset for
+// which the given client owns the script keys.
+func (t *TapdClient) GetUnEncumberedAssetBalance(ctx context.Context,
+	assetID []byte) (uint64, error) {
+
+	allAssets, err := t.ListAssets(ctx, &taprpc.ListAssetRequest{})
+	if err != nil {
+		return 0, err
+	}
+
+	var balance uint64
+	for _, a := range allAssets.Assets {
+		// Only count assets from the given asset ID.
+		if !bytes.Equal(a.AssetGenesis.AssetId, assetID) {
+			continue
+		}
+
+		// Non-local means we don't have the internal key to spend the
+		// asset.
+		if !a.ScriptKeyIsLocal {
+			continue
+		}
+
+		// If the asset is not declared known or has a script path, we
+		// can't spend it directly.
+		if !a.ScriptKeyDeclaredKnown || a.ScriptKeyHasScriptPath {
+			continue
+		}
+
+		balance += a.Amount
+	}
+
+	return balance, nil
+}
+
+// DeriveNewKeys derives a new internal and script key.
+func (t *TapdClient) DeriveNewKeys(ctx context.Context) (asset.ScriptKey,
+	keychain.KeyDescriptor, error) {
+
+	scriptKeyDesc, err := t.NextScriptKey(
+		ctx, &assetwalletrpc.NextScriptKeyRequest{
+			KeyFamily: uint32(asset.TaprootAssetsKeyFamily),
+		},
+	)
+	if err != nil {
+		return asset.ScriptKey{}, keychain.KeyDescriptor{}, err
+	}
+
+	scriptKey, err := rpcutils.UnmarshalScriptKey(scriptKeyDesc.ScriptKey)
+	if err != nil {
+		return asset.ScriptKey{}, keychain.KeyDescriptor{}, err
+	}
+
+	internalKeyDesc, err := t.NextInternalKey(
+		ctx, &assetwalletrpc.NextInternalKeyRequest{
+			KeyFamily: uint32(asset.TaprootAssetsKeyFamily),
+		},
+	)
+	if err != nil {
+		return asset.ScriptKey{}, keychain.KeyDescriptor{}, err
+	}
+	internalKeyLnd, err := rpcutils.UnmarshalKeyDescriptor(
+		internalKeyDesc.InternalKey,
+	)
+	if err != nil {
+		return asset.ScriptKey{}, keychain.KeyDescriptor{}, err
+	}
+
+	return *scriptKey, internalKeyLnd, nil
+}
+
+// ImportProofFile imports the proof file and returns the last proof.
+func (t *TapdClient) ImportProofFile(ctx context.Context, rawProofFile []byte) (
+	*proof.Proof, error) {
+
+	proofFile, err := proof.DecodeFile(rawProofFile)
+	if err != nil {
+		return nil, err
+	}
+
+	var lastProof *proof.Proof
+
+	for i := 0; i < proofFile.NumProofs(); i++ {
+		lastProof, err = proofFile.ProofAt(uint32(i))
+		if err != nil {
+			return nil, err
+		}
+
+		var proofBytes bytes.Buffer
+		err = lastProof.Encode(&proofBytes)
+		if err != nil {
+			return nil, err
+		}
+
+		asset := lastProof.Asset
+
+		proofType := universe.ProofTypeTransfer
+		if asset.IsGenesisAsset() {
+			proofType = universe.ProofTypeIssuance
+		}
+
+		uniID := universe.Identifier{
+			AssetID:   asset.ID(),
+			ProofType: proofType,
+		}
+		if asset.GroupKey != nil {
+			uniID.GroupKey = &asset.GroupKey.GroupPubKey
+		}
+
+		rpcUniID, err := tap.MarshalUniID(uniID)
+		if err != nil {
+			return nil, err
+		}
+
+		outpoint := &universerpc.Outpoint{
+			HashStr: lastProof.AnchorTx.TxHash().String(),
+			Index:   int32(lastProof.InclusionProof.OutputIndex),
+		}
+
+		scriptKey := lastProof.Asset.ScriptKey.PubKey
+		leafKey := &universerpc.AssetKey{
+			Outpoint: &universerpc.AssetKey_Op{
+				Op: outpoint,
+			},
+			ScriptKey: &universerpc.AssetKey_ScriptKeyBytes{
+				ScriptKeyBytes: scriptKey.SerializeCompressed(),
+			},
+		}
+
+		_, err = t.InsertProof(ctx, &universerpc.AssetProof{
+			Key: &universerpc.UniverseKey{
+				Id:      rpcUniID,
+				LeafKey: leafKey,
+			},
+			AssetLeaf: &universerpc.AssetLeaf{
+				Proof: proofBytes.Bytes(),
+			},
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return lastProof, nil
+}
+
+func (t *TapdClient) AddHoldInvoice(ctx context.Context, pHash lntypes.Hash,
+	assetId []byte, assetAmt uint64, memo string) (
+	*tapchannelrpc.AddInvoiceResponse, error) {
+
+	// Now we can create the swap invoice.
+	invoiceRes, err := t.AddInvoice(
+		ctx, &tapchannelrpc.AddInvoiceRequest{
+
+			// Todo(sputn1ck):if we have more than one peer, we'll need to
+			// specify one. This will likely be changed on the tapd front in
+			// the future.
+			PeerPubkey:  nil,
+			AssetId:     assetId,
+			AssetAmount: assetAmt,
+			InvoiceRequest: &lnrpc.Invoice{
+				Memo:  memo,
+				RHash: pHash[:],
+				// todo fix expiries
+				CltvExpiry: 144,
+				Expiry:     60,
+				Private:    true,
+			},
+			HodlInvoice: &tapchannelrpc.HodlInvoice{
+				PaymentHash: pHash[:],
+			},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return invoiceRes, nil
+}
+
+func (t *TapdClient) SendPayment(ctx context.Context,
+	invoice string, assetId []byte) (chan *tapchannelrpc.SendPaymentResponse,
+	chan error, error) {
+
+	req := &routerrpc.SendPaymentRequest{
+		PaymentRequest: invoice,
+	}
+
+	sendReq := &tapchannelrpc.SendPaymentRequest{
+		AssetId:        assetId,
+		PaymentRequest: req,
+	}
+
+	sendResp, err := t.TaprootAssetChannelsClient.SendPayment(
+		ctx, sendReq,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	sendRespChan := make(chan *tapchannelrpc.SendPaymentResponse)
+	errChan := make(chan error)
+	go func() {
+		defer close(sendRespChan)
+		defer close(errChan)
+		for {
+			select {
+			case <-ctx.Done():
+				errChan <- ctx.Err()
+				return
+			default:
+				res, err := sendResp.Recv()
+				if err != nil {
+					errChan <- err
+					return
+				}
+				sendRespChan <- res
+			}
+		}
+	}()
+
+	return sendRespChan, errChan, nil
 }
