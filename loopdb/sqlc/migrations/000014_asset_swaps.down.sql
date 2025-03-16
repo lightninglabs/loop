3ac7b7ea797306eb19aee417c5ae3f9477b5b8c1
sputn1ck <kon@kon.ninja>
2025-03-15 21:24:11 -0500

    loopdb: add asset out migrations

---
+++ loopdb/sqlc/migrations/000014_asset_swaps.down.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS asset_out_swaps_swap_hash_idx;
+DROP TABLE IF EXISTS asset_out_swaps;
+DROP INDEX IF EXISTS asset_swaps_updates_swap_hash_idx;
+DROP TABLE IF EXISTS asset_swaps;
+DROP TABLE IF EXISTS asset_swaps;
