fb31e064b9598ef381a7fee276a55443abfb9c49
Slyghtning <hieblmi@protonmail.com>
2025-05-21 14:13:53 +0200

    staticutil: channel open functions

--- staticaddr/staticutil/utils.go
+++ staticaddr/staticutil/utils.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"sort"
 
+	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/lightninglabs/lndclient"
@@ -14,6 +15,7 @@ import (
 	"github.com/lightninglabs/loop/staticaddr/script"
 	"github.com/lightninglabs/loop/swapserverrpc"
 	"github.com/lightningnetwork/lnd/input"
+	"github.com/lightningnetwork/lnd/lnwallet"
 )
 
 // ToPrevOuts converts a slice of deposits to a map of outpoints to TxOuts.
@@ -64,6 +66,36 @@ func CreateMusig2Sessions(ctx context.Context,
 	}
 
 	return musig2Sessions, clientNonces, nil
+}
+
+// CreateMusig2SessionsPerDeposit creates a musig2 session for a number of
+// deposits.
+func CreateMusig2SessionsPerDeposit(ctx context.Context,
+	signer lndclient.SignerClient, deposits []*deposit.Deposit,
+	addrParams *address.Parameters,
+	staticAddress *script.StaticAddress) (
+	map[string]*input.MuSig2SessionInfo, map[string][]byte, map[string]int,
+	error) {
+
+	sessions := make(map[string]*input.MuSig2SessionInfo)
+	nonces := make(map[string][]byte)
+	depositToIdx := make(map[string]int)
+
+	// Create the musig2 sessions for the sweepless sweep tx.
+	for i, deposit := range deposits {
+		session, err := CreateMusig2Session(
+			ctx, signer, addrParams, staticAddress,
+		)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+
+		sessions[deposit.String()] = session
+		nonces[deposit.String()] = session.PublicNonce[:]
+		depositToIdx[deposit.String()] = i
+	}
+
+	return sessions, nonces, depositToIdx, nil
 }
 
 // CreateMusig2Session creates a musig2 session for the deposit.
@@ -131,4 +163,43 @@ func bip69inputLess(input1, input2 *swapserverrpc.PrevoutInfo) bool {
 		jhash[b], jhash[hashSize-1-b] = jhash[hashSize-1-b], jhash[b]
 	}
 	return bytes.Compare(ihash[:], jhash[:]) == -1
+}
+
+// SelectDeposits sorts the deposits by amount in descending order. It then
+// selects the deposits that are needed to cover the amount requested without
+// leaving a dust change. It returns an error if the sum of deposits minus dust
+// is less than the requested amount.
+func SelectDeposits(deposits []*deposit.Deposit, amount int64) (
+	[]*deposit.Deposit, error) {
+
+	// Check that sum of deposits covers the swap amount while leaving no
+	// dust change.
+	dustLimit := lnwallet.DustLimitForSize(input.P2TRSize)
+	var depositSum btcutil.Amount
+	for _, deposit := range deposits {
+		depositSum += deposit.Value
+	}
+	if depositSum-dustLimit < btcutil.Amount(amount) {
+		return nil, fmt.Errorf("insufficient funds to cover swap " +
+			"amount, try manually selecting deposits")
+	}
+
+	// Sort the deposits by amount in descending order.
+	sort.Slice(deposits, func(i, j int) bool {
+		return deposits[i].Value > deposits[j].Value
+	})
+
+	// Select the deposits that are needed to cover the swap amount without
+	// leaving a dust change.
+	var selectedDeposits []*deposit.Deposit
+	var selectedAmount btcutil.Amount
+	for _, deposit := range deposits {
+		if selectedAmount >= btcutil.Amount(amount)+dustLimit {
+			break
+		}
+		selectedDeposits = append(selectedDeposits, deposit)
+		selectedAmount += deposit.Value
+	}
+
+	return selectedDeposits, nil
 }
