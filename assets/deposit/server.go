948d2df9002514afcea814cc40fe5ace87b5b765
Andras Banki-Horvath <bhandras@gmail.com>
2025-07-15 13:59:41 +0200

    assets+loopdb: implement new asset deposit functionality
    This commit extends the asset deposit manager along with the underlying
    sql store, adding functionality to create and fund new asset deposits.

--- assets/deposit/server.go
+++ assets/deposit/server.go
@@ -2,10 +2,20 @@ package deposit
 
 import (
 	"context"
+	"encoding/hex"
+	"fmt"
 
 	"github.com/lightninglabs/loop/looprpc"
+	"github.com/lightninglabs/taproot-assets/asset"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+)
+
+var (
+	// ErrAssetDepositsUnavailable is returned when the asset deposit
+	// service is not available.
+	ErrAssetDepositsUnavailable = status.Error(codes.Unavailable,
+		"asset deposits are unavailable")
 )
 
 // Server is the grpc server that serves the reservation service.
@@ -27,7 +37,44 @@ func (s *Server) NewAssetDeposit(ctx context.Context,
 	in *looprpc.NewAssetDepositRequest) (*looprpc.NewAssetDepositResponse,
 	error) {
 
-	return nil, status.Error(codes.Unimplemented, "unimplemented")
+	if s.manager == nil {
+		return nil, ErrAssetDepositsUnavailable
+	}
+
+	assetIDBytes, err := hex.DecodeString(in.AssetId)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument,
+			fmt.Sprintf("invalid asset ID encoding: %v", err))
+	}
+
+	var assetID asset.ID
+	if len(assetIDBytes) != len(assetID) {
+		return nil, fmt.Errorf("invalid asset ID lenght: expected "+
+			"%v bytes, got %d", len(assetID), len(assetIDBytes))
+	}
+
+	copy(assetID[:], assetIDBytes)
+
+	if in.Amount == 0 {
+		return nil, status.Error(codes.InvalidArgument,
+			"amount must be greater than zero")
+	}
+
+	if in.CsvExpiry <= 0 {
+		return nil, status.Error(codes.InvalidArgument,
+			"CSV expiry must be greater than zero")
+	}
+
+	depositInfo, err := s.manager.NewDeposit(
+		ctx, assetID, in.Amount, uint32(in.CsvExpiry),
+	)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &looprpc.NewAssetDepositResponse{
+		DepositId: depositInfo.ID,
+	}, nil
 }
 
 // ListAssetDeposits is the rpc endpoint for loop clients to list their asset
--- assets/deposit/sql_store.go
+++ assets/deposit/sql_store.go
@@ -2,10 +2,12 @@ package deposit
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/lightninglabs/loop/loopdb"
+	"github.com/lightninglabs/loop/loopdb/sqlc"
 	"github.com/lightninglabs/taproot-assets/address"
 	"github.com/lightningnetwork/lnd/clock"
 )
@@ -13,7 +15,13 @@ import (
 // Querier is a subset of the methods we need from the postgres.querier
 // interface for the deposit store.
 type Querier interface {
-	// This is intentionally left empty.
+	AddAssetDeposit(context.Context, sqlc.AddAssetDepositParams) error
+
+	UpdateDepositState(ctx context.Context,
+		arg sqlc.UpdateDepositStateParams) error
+
+	MarkDepositConfirmed(ctx context.Context,
+		arg sqlc.MarkDepositConfirmedParams) error
 }
 
 // DepositBaseDB is the interface that contains all the queries generated
@@ -47,8 +55,83 @@ func NewSQLStore(db DepositBaseDB, clock clock.Clock,
 	}
 }
 
+// AddAssetDeposit adds a new asset deposit to the database.
+func (s *SQLStore) AddAssetDeposit(ctx context.Context, d *Deposit) error {
+	txOptions := loopdb.NewSqlWriteOpts()
+
+	createdAt := d.CreatedAt.UTC()
+	clientScriptPubKey := d.FunderScriptKey.SerializeCompressed()
+	clientInternalPubKey := d.FunderInternalKey.SerializeCompressed()
+	serverScriptPubKey := d.CoSignerScriptKey.SerializeCompressed()
+	serverInternalPubKey := d.CoSignerInternalKey.SerializeCompressed()
+
+	return s.db.ExecTx(ctx, txOptions, func(tx Querier) error {
+		err := tx.AddAssetDeposit(ctx, sqlc.AddAssetDepositParams{
+			DepositID:            d.ID,
+			CreatedAt:            createdAt,
+			AssetID:              d.AssetID[:],
+			Amount:               int64(d.Amount),
+			ClientScriptPubkey:   clientScriptPubKey,
+			ClientInternalPubkey: clientInternalPubKey,
+			ServerScriptPubkey:   serverScriptPubKey,
+			ServerInternalPubkey: serverInternalPubKey,
+			ClientKeyFamily:      int32(d.KeyLocator.Family),
+			ClientKeyIndex:       int32(d.KeyLocator.Index),
+			Expiry:               int32(d.CsvExpiry),
+			Addr:                 d.Addr,
+			ProtocolVersion:      int32(d.Version),
+		})
+		if err != nil {
+			return err
+		}
+
+		return tx.UpdateDepositState(ctx, sqlc.UpdateDepositStateParams{
+			DepositID:       d.ID,
+			UpdateState:     int32(StateInitiated),
+			UpdateTimestamp: createdAt,
+		})
+	})
+}
+
 // UpdateDeposit updates the deposit state and extends the depsoit update log
 // the SQL store.
 func (s *SQLStore) UpdateDeposit(ctx context.Context, d *Deposit) error {
-	return nil
+	txOptions := loopdb.NewSqlWriteOpts()
+
+	return s.db.ExecTx(ctx, txOptions, func(tx Querier) error {
+		switch d.State {
+		case StateConfirmed:
+			err := tx.MarkDepositConfirmed(
+				ctx, sqlc.MarkDepositConfirmedParams{
+					DepositID: d.ID,
+					ConfirmationHeight: sql.NullInt32{
+						Int32: int32(
+							d.ConfirmationHeight,
+						),
+						Valid: true,
+					},
+					Outpoint: sql.NullString{
+						String: d.Outpoint.String(),
+						Valid:  true,
+					},
+					PkScript: d.PkScript,
+				},
+			)
+			if err != nil {
+				return err
+			}
+
+		default:
+			return fmt.Errorf("unimplemented deposit state "+
+				"update: %v", d.State)
+		}
+
+		return tx.UpdateDepositState(
+			ctx, sqlc.UpdateDepositStateParams{
+				DepositID:       d.ID,
+				UpdateState:     int32(d.State),
+				UpdateTimestamp: s.clock.Now().UTC(),
+			},
+		)
+	})
 }
