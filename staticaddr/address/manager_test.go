d9cf80857c8bb74ce799bac8d59c25f4930e7822
Slyghtning <hieblmi@protonmail.com>
2025-08-28 15:22:52 +0200

    staticaddr: serialize address creation with a channel

--- staticaddr/address/manager_test.go
+++ staticaddr/address/manager_test.go
@@ -119,14 +119,19 @@ func TestManager(t *testing.T) {
 	require.NoError(t, err)
 
 	// Create a new static address.
-	taprootAddress, expiry, err := testContext.manager.NewAddress(ctxb)
+	params, err := testContext.manager.NewAddress(ctxb)
+	require.NoError(t, err)
+
+	address, err := testContext.manager.GetTaprootAddress(
+		params.ClientPubkey, params.ServerPubkey, int64(params.Expiry),
+	)
 	require.NoError(t, err)
 
 	// The addresses have to match.
-	require.Equal(t, expectedAddress.String(), taprootAddress.String())
+	require.Equal(t, expectedAddress.String(), address.String())
 
 	// The expiry has to match.
-	require.EqualValues(t, defaultExpiry, expiry)
+	require.EqualValues(t, defaultExpiry, params.Expiry)
 }
 
 // GenerateExpectedTaprootAddress generates the expected taproot address that
