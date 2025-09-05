37b7e08be36360387e2f4001320a47f11fd368ed
Slyghtning <hieblmi@protonmail.com>
2025-09-05 10:32:57 +0200

    loopdb: load static address params as part of deposits

--- loopdb/sqlc/static_address_deposits.sql.go
+++ loopdb/sqlc/static_address_deposits.sql.go
@@ -13,22 +13,51 @@ import (
 
 const allDeposits = `-- name: AllDeposits :many
 SELECT
-    id, deposit_id, tx_hash, out_index, amount, confirmation_height, timeout_sweep_pk_script, expiry_sweep_txid, finalized_withdrawal_tx, swap_hash, static_address_id
-FROM
-    deposits
-ORDER BY
-    id ASC
+    d.id, d.deposit_id, d.tx_hash, d.out_index, d.amount, d.confirmation_height, d.timeout_sweep_pk_script, d.expiry_sweep_txid, d.finalized_withdrawal_tx, d.swap_hash, d.static_address_id,
+    sa.client_pubkey     client_pubkey,
+    sa.server_pubkey     server_pubkey,
+    sa.expiry            expiry,
+    sa.client_key_family client_key_family,
+    sa.client_key_index  client_key_index,
+    sa.pkscript          pkscript,
+    sa.protocol_version  protocol_version,
+    sa.initiation_height initiation_height
+FROM deposits d
+         LEFT JOIN static_addresses sa ON sa.id = d.static_address_id
+ORDER BY d.id ASC
 `
 
-func (q *Queries) AllDeposits(ctx context.Context) ([]Deposit, error) {
+type AllDepositsRow struct {
+	ID                    int32
+	DepositID             []byte
+	TxHash                []byte
+	OutIndex              int32
+	Amount                int64
+	ConfirmationHeight    int64
+	TimeoutSweepPkScript  []byte
+	ExpirySweepTxid       []byte
+	FinalizedWithdrawalTx sql.NullString
+	SwapHash              []byte
+	StaticAddressID       sql.NullInt32
+	ClientPubkey          []byte
+	ServerPubkey          []byte
+	Expiry                sql.NullInt32
+	ClientKeyFamily       sql.NullInt32
+	ClientKeyIndex        sql.NullInt32
+	Pkscript              []byte
+	ProtocolVersion       sql.NullInt32
+	InitiationHeight      sql.NullInt32
+}
+
+func (q *Queries) AllDeposits(ctx context.Context) ([]AllDepositsRow, error) {
 	rows, err := q.db.QueryContext(ctx, allDeposits)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
-	var items []Deposit
+	var items []AllDepositsRow
 	for rows.Next() {
-		var i Deposit
+		var i AllDepositsRow
 		if err := rows.Scan(
 			&i.ID,
 			&i.DepositID,
@@ -41,6 +70,14 @@ func (q *Queries) AllDeposits(ctx context.Context) ([]Deposit, error) {
 			&i.FinalizedWithdrawalTx,
 			&i.SwapHash,
 			&i.StaticAddressID,
+			&i.ClientPubkey,
+			&i.ServerPubkey,
+			&i.Expiry,
+			&i.ClientKeyFamily,
+			&i.ClientKeyIndex,
+			&i.Pkscript,
+			&i.ProtocolVersion,
+			&i.InitiationHeight,
 		); err != nil {
 			return nil, err
 		}
@@ -64,7 +101,9 @@ INSERT INTO deposits (
     confirmation_height,
     timeout_sweep_pk_script,
     expiry_sweep_txid,
-    finalized_withdrawal_tx
+    finalized_withdrawal_tx,
+                      static_address_id
+
 ) VALUES (
              $1,
              $2,
@@ -73,7 +112,8 @@ INSERT INTO deposits (
              $5,
              $6,
              $7,
-             $8
+             $8,
+             $9
          )
 `
 
@@ -86,6 +126,7 @@ type CreateDepositParams struct {
 	TimeoutSweepPkScript  []byte
 	ExpirySweepTxid       []byte
 	FinalizedWithdrawalTx sql.NullString
+	StaticAddressID       sql.NullInt32
 }
 
 func (q *Queries) CreateDeposit(ctx context.Context, arg CreateDepositParams) error {
@@ -98,15 +139,24 @@ func (q *Queries) CreateDeposit(ctx context.Context, arg CreateDepositParams) er
 		arg.TimeoutSweepPkScript,
 		arg.ExpirySweepTxid,
 		arg.FinalizedWithdrawalTx,
+		arg.StaticAddressID,
 	)
 	return err
 }
 
 const depositForOutpoint = `-- name: DepositForOutpoint :one
 SELECT
-    id, deposit_id, tx_hash, out_index, amount, confirmation_height, timeout_sweep_pk_script, expiry_sweep_txid, finalized_withdrawal_tx, swap_hash, static_address_id
-FROM
-    deposits
+    d.id, d.deposit_id, d.tx_hash, d.out_index, d.amount, d.confirmation_height, d.timeout_sweep_pk_script, d.expiry_sweep_txid, d.finalized_withdrawal_tx, d.swap_hash, d.static_address_id,
+    sa.client_pubkey     client_pubkey,
+    sa.server_pubkey     server_pubkey,
+    sa.expiry            expiry,
+    sa.client_key_family client_key_family,
+    sa.client_key_index  client_key_index,
+    sa.pkscript          pkscript,
+    sa.protocol_version  protocol_version,
+    sa.initiation_height initiation_height
+FROM deposits d
+         LEFT JOIN static_addresses sa ON sa.id = d.static_address_id
 WHERE
     tx_hash = $1
 AND
@@ -118,9 +168,31 @@ type DepositForOutpointParams struct {
 	OutIndex int32
 }
 
-func (q *Queries) DepositForOutpoint(ctx context.Context, arg DepositForOutpointParams) (Deposit, error) {
+type DepositForOutpointRow struct {
+	ID                    int32
+	DepositID             []byte
+	TxHash                []byte
+	OutIndex              int32
+	Amount                int64
+	ConfirmationHeight    int64
+	TimeoutSweepPkScript  []byte
+	ExpirySweepTxid       []byte
+	FinalizedWithdrawalTx sql.NullString
+	SwapHash              []byte
+	StaticAddressID       sql.NullInt32
+	ClientPubkey          []byte
+	ServerPubkey          []byte
+	Expiry                sql.NullInt32
+	ClientKeyFamily       sql.NullInt32
+	ClientKeyIndex        sql.NullInt32
+	Pkscript              []byte
+	ProtocolVersion       sql.NullInt32
+	InitiationHeight      sql.NullInt32
+}
+
+func (q *Queries) DepositForOutpoint(ctx context.Context, arg DepositForOutpointParams) (DepositForOutpointRow, error) {
 	row := q.db.QueryRowContext(ctx, depositForOutpoint, arg.TxHash, arg.OutIndex)
-	var i Deposit
+	var i DepositForOutpointRow
 	err := row.Scan(
 		&i.ID,
 		&i.DepositID,
@@ -133,22 +205,60 @@ func (q *Queries) DepositForOutpoint(ctx context.Context, arg DepositForOutpoint
 		&i.FinalizedWithdrawalTx,
 		&i.SwapHash,
 		&i.StaticAddressID,
+		&i.ClientPubkey,
+		&i.ServerPubkey,
+		&i.Expiry,
+		&i.ClientKeyFamily,
+		&i.ClientKeyIndex,
+		&i.Pkscript,
+		&i.ProtocolVersion,
+		&i.InitiationHeight,
 	)
 	return i, err
 }
 
 const getDeposit = `-- name: GetDeposit :one
 SELECT
-    id, deposit_id, tx_hash, out_index, amount, confirmation_height, timeout_sweep_pk_script, expiry_sweep_txid, finalized_withdrawal_tx, swap_hash, static_address_id
-FROM
-    deposits
+    d.id, d.deposit_id, d.tx_hash, d.out_index, d.amount, d.confirmation_height, d.timeout_sweep_pk_script, d.expiry_sweep_txid, d.finalized_withdrawal_tx, d.swap_hash, d.static_address_id,
+    sa.client_pubkey     client_pubkey,
+    sa.server_pubkey     server_pubkey,
+    sa.expiry            expiry,
+    sa.client_key_family client_key_family,
+    sa.client_key_index  client_key_index,
+    sa.pkscript          pkscript,
+    sa.protocol_version  protocol_version,
+    sa.initiation_height initiation_height
+FROM deposits d
+         LEFT JOIN static_addresses sa ON sa.id = d.static_address_id
 WHERE
     deposit_id = $1
 `
 
-func (q *Queries) GetDeposit(ctx context.Context, depositID []byte) (Deposit, error) {
+type GetDepositRow struct {
+	ID                    int32
+	DepositID             []byte
+	TxHash                []byte
+	OutIndex              int32
+	Amount                int64
+	ConfirmationHeight    int64
+	TimeoutSweepPkScript  []byte
+	ExpirySweepTxid       []byte
+	FinalizedWithdrawalTx sql.NullString
+	SwapHash              []byte
+	StaticAddressID       sql.NullInt32
+	ClientPubkey          []byte
+	ServerPubkey          []byte
+	Expiry                sql.NullInt32
+	ClientKeyFamily       sql.NullInt32
+	ClientKeyIndex        sql.NullInt32
+	Pkscript              []byte
+	ProtocolVersion       sql.NullInt32
+	InitiationHeight      sql.NullInt32
+}
+
+func (q *Queries) GetDeposit(ctx context.Context, depositID []byte) (GetDepositRow, error) {
 	row := q.db.QueryRowContext(ctx, getDeposit, depositID)
-	var i Deposit
+	var i GetDepositRow
 	err := row.Scan(
 		&i.ID,
 		&i.DepositID,
@@ -161,6 +271,14 @@ func (q *Queries) GetDeposit(ctx context.Context, depositID []byte) (Deposit, er
 		&i.FinalizedWithdrawalTx,
 		&i.SwapHash,
 		&i.StaticAddressID,
+		&i.ClientPubkey,
+		&i.ServerPubkey,
+		&i.Expiry,
+		&i.ClientKeyFamily,
+		&i.ClientKeyIndex,
+		&i.Pkscript,
+		&i.ProtocolVersion,
+		&i.InitiationHeight,
 	)
 	return i, err
 }
