25485226e4c62cc4a1d110b0e2dea49b5c03d004
Slyghtning <hieblmi@protonmail.com>
2025-10-01 08:52:56 +0200

    cmd: staticaddr fast-flag support

--- cmd/loop/staticaddr.go
+++ cmd/loop/staticaddr.go
@@ -485,6 +485,12 @@ var staticAddressLoopInCommand = cli.Command{
 				"is change it is sent back to the static " +
 				"address.",
 		},
+		cli.BoolFlag{
+			Name: "fast",
+			Usage: "Usage: complete the swap faster by paying a " +
+				"higher fee, so the change output is " +
+				"available sooner",
+		},
 		lastHopFlag,
 		labelFlag,
 		routeHintsFlag,
@@ -573,7 +579,8 @@ func staticAddressLoopIn(ctx *cli.Context) error {
 		depositOutpoints = ctx.StringSlice("utxo")
 
 	case selectedAmount > 0:
-		// If only an amount is selected we will trigger coin selection.
+		// If only an amount is selected, we will trigger coin
+		// selection.
 
 	default:
 		return fmt.Errorf("unknown quote request")
@@ -594,6 +601,7 @@ func staticAddressLoopIn(ctx *cli.Context) error {
 		Private:            ctx.Bool(privateFlag.Name),
 		DepositOutpoints:   depositOutpoints,
 		AutoSelectDeposits: autoSelectDepositsForQuote,
+		Fast:               ctx.Bool("fast"),
 	}
 	quote, err := client.GetLoopInQuote(ctxb, quoteReq)
 	if err != nil {
@@ -623,6 +631,7 @@ func staticAddressLoopIn(ctx *cli.Context) error {
 		RouteHints:            hints,
 		Private:               ctx.Bool("private"),
 		PaymentTimeoutSeconds: paymentTimeoutSeconds,
+		Fast:                  ctx.Bool("fast"),
 	}
 
 	resp, err := client.StaticAddressLoopIn(ctxb, req)
