7cab89d45b4b86165cf02eee578a028ec621953d
Slyghtning <hieblmi@protonmail.com>
2026-08-11 11:30:10 +0200

    looprpc: align instant out permissions with loop out
    Apply the loop:out permission to Instant Out and reservation RPCs so
    their authorization requirements match the rest of the Loop Out API.

--- looprpc/perms.go
+++ looprpc/perms.go
@@ -177,18 +177,30 @@ var RequiredPermissions = map[string][]bakery.Op{
 	"/looprpc.SwapClient/ListReservations": {{
 		Entity: "swap",
 		Action: "read",
+	}, {
+		Entity: "loop",
+		Action: "out",
 	}},
 	"/looprpc.SwapClient/InstantOut": {{
 		Entity: "swap",
 		Action: "execute",
+	}, {
+		Entity: "loop",
+		Action: "out",
 	}},
 	"/looprpc.SwapClient/InstantOutQuote": {{
 		Entity: "swap",
 		Action: "read",
+	}, {
+		Entity: "loop",
+		Action: "out",
 	}},
 	"/looprpc.SwapClient/ListInstantOuts": {{
 		Entity: "swap",
 		Action: "read",
+	}, {
+		Entity: "loop",
+		Action: "out",
 	}},
 	"/looprpc.SwapClient/StopDaemon": {{
 		Entity: "loop",
